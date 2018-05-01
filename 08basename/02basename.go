0b867ec5ef3d8d5d6ec09887be6186d5e2a59d60
Archy <254771771@qq.com>
2018-05-01 23:41:58 +0800

    basename

---
+++ 08basename/02basename.go
@@ -0,0 +1,10 @@
+func basename(s string) string {
+    slash := strings.LastIndex(s, "/") 
+    s = s[slash+1:]
+    if dot := strings.LastIndex(s, "."); dot >= 0 {
+        s = s[:dot]
+        134
+    }
+    return s
+}
+
